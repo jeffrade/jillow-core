856421fca4acdf78e8c91965052d98b902c76d6c
jer3321 <jeffrade@gmail.com>
2014-08-09 09:12:17 -0500

    Added new enum for chart param; renamed test class

---
+++ src/main/java/com/github/jillow/util/ChartUnitType.java
@@ -0,0 +1,14 @@
+package com.github.jillow.util;
+
+public enum ChartUnitType {
+	
+	PERCENT,
+	
+	DOLLAR;
+	
+	@Override
+	public String toString(){
+		return super.toString().toLowerCase();
+	}
+
+}
